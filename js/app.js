665cbab7f2c3aee8aca99412560c7b45e54dc7ac
ximena <medinaximena98@gmail.com>
2019-10-30 18:51:24 -0500

    Avance CRUD Firebase

--- js/app.js
+++ js/app.js
@@ -1,29 +1,41 @@
 
-var user = document.getElementById('user');
-var pass = document.getElementById('pass');
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBBef4UHTtd4-dJzRgrfoZcm-haFVUIGeU",
+    authDomain: "proyecto-bioteca.firebaseapp.com",
+    databaseURL: "https://proyecto-bioteca.firebaseio.com",
+    projectId: "proyecto-bioteca",
+    storageBucket: "proyecto-bioteca.appspot.com",
+    messagingSenderId: "357420235789",
+    appId: "1:357420235789:web:7bdd709aab91a6deb36944"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-function inicio() {
-    if (user.value.length == 0 || pass.value.length == 0) {
-        alert("Incompleto")
-    } else {
-        if (user.value == "admin" && pass.value == "admin") {
-            window.location = "inicioadministrador.html";
-            alert("correcto");
-            console.log("Usuario: " + user.value);
-            console.log("Contraseña: " + pass.value);
-            console.log("Credenciales validas");
+var db = firebase.firestore();
 
-        } else {
-            if (user.value == "usuario" && pass.value == "usuario") {
-                window.location = "index.html";
-                alert("correcto");
-                console.log("Usuario: " + user.value);
-                console.log("Contraseña: " + pass.value);
-                console.log("Credenciales validas");
+var Nombres = document.getElementById('Nombres');
+var Apellidos = document.getElementById('Apellidos');
+var Correo = document.getElementById('Correo');
+var Contrasena = document.getElementById('Contrasena');
+var RContrasena = document.getElementById('RContrasena');
 
-            } else {
-                alert("usuario y/o contraseña incorrectos");
-            }
-        }
-    }
+function InsertarUsuario() {
+    alert("holi");
+    db.collection("Usuarios").add({
+        Nombres: Nombres.value,
+        Apellidos: Apellidos.value,
+        Correo: Correo.value,
+        Contrasena: Contrasena.value,
+        RContrasena: RContrasena.value
+    })
+        .then(function (docRef) {
+            console.log("Usuario agregado", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error", error);
+        });
 }
+
+
+
